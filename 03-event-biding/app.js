f85a45fc58c81b630ca494cbc298a5a100cb07f3
Anthony <anthony.rousseau26@gmail.com>
2024-03-11 16:03:51 +0100

    Practicing vue basics

--- 03-event-biding/app.js
+++ 03-event-biding/app.js
@@ -2,8 +2,43 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: '',
     };
   },
+  methods: {
+    add(num) {
+      this.counter += num;
+    },
+    reduce(num) {
+      this.counter -= num;
+    },
+    setName(event, lastname) {
+      this.name = event.target.value;
+    },
+    submitForm() {
+      alert('Submitted!');
+    },
+    confirmInput() {
+      this.confirmedName = this.name;
+    },
+    resetInput() {
+      this.name = '';
+    },
+    outputFullname(){
+      if(this.name === ''){
+        return '';
+      }
+      return this.name + ' ' + 'Lastname';
+    }
+  },
+  computed:{
+    // fullname() {
+    //   if(this.name === ''){
+    //     return '';
+    //   }
+    //   return this.name + ' ' + 'Lastname'; 
+    // }
+  }
 });
 
 app.mount('#events');
