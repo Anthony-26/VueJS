fa534bc6025140cc0298eee524d778d355434339
Anthony <anthony.rousseau26@gmail.com>
2024-03-12 17:23:26 +0100

    Implementing app logic - method and computed

--- 06-monster-slayer-game/app.js
+++ 06-monster-slayer-game/app.js
@@ -1,9 +1,33 @@
+function getRandomValue(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
 const app = Vue.createApp({
   data() {
     return {
-      
-    }
+      playerHealth: 100,
+      monsterHealth: 100,
+    };
   },
-})
+
+  methods: {
+    attackMonster() {
+      this.monsterHealth -= getRandomValue(5, 12);
+      this.attackPlayer();
+    },
+    attackPlayer() {
+      this.playerHealth -= getRandomValue(5, 13);
+    },
+  },
+
+  computed: {
+    monsterBarStyles() {
+      return { width: this.monsterHealth + '%' };
+    },
+    playerBarStyles() {
+      return { width: this.playerHealth + '%' };
+    },
+  },
+});
 
 app.mount('#game');
