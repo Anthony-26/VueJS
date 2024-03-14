cd5b618295d5e3adc59b5dd7a7ee11999dae29cd
Anthony <anthony.rousseau26@gmail.com>
2024-03-14 21:55:29 +0100

    Test to implement component

---
+++ 07-components/app.js
@@ -0,0 +1,53 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      friends: [
+        {
+          id: 'manuel',
+          name: 'Manuel Lorenz',
+          phone: '01234 5678 991',
+          email: 'manuellorenz@gmail.com',
+        },
+        {
+          id: 'julie',
+          name: 'Julie Jones',
+          phone: '98745 6534 2121',
+          email: 'juliejones@gmail.com',
+        },
+      ],
+    };
+  },
+});
+
+app.component('friend-contact', {
+  template: `
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails()">
+      {{ detailsAreVisible ? 'Hide' : 'Show' }} Details
+    </button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong> {{ friend.phone }}</li>
+      <li><strong>Email:</strong> {{ friend.email }}</li>
+    </ul>
+  </li>
+  `,
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: 'manuel',
+        name: 'Manuel Lorenz',
+        phone: '01234 5678 991',
+        email: 'manuellorenz@gmail.com',
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+  },
+});
+
+app.mount('#app');
