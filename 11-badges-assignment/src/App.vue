c85b92eff87b49ef8a3ca0d75f4cbd5e2e6920c6
Anthony <anthony.rousseau26@gmail.com>
2024-03-25 21:45:07 +0100

    Implementing local components notion

--- 11-badges-assignment/src/App.vue
+++ 11-badges-assignment/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <the-header></the-header>
+    <TheHeader />
+    <!-- <the-header></the-header> -> Alternative -->
     <badge-list></badge-list>
     <user-info
       :full-name="activeUser.name"
@@ -11,7 +12,17 @@
 </template>
 
 <script>
+import TheHeader from './components/TheHeader.vue';
+import BadgeList from './components/BadgeList.vue';
+import UserInfo from './components/UserInfo.vue';
+
 export default {
+  components: {
+    TheHeader,
+    BadgeList,
+    UserInfo
+    // the-header: TheHeader, -> Alternative
+  },
   data() {
     return {
       activeUser: {
