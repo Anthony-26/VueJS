9b7e56bf8104b5b98b0d74e5d558e94a7177a2cd
Anthony <anthony.rousseau26@gmail.com>
2024-03-26 11:30:58 +0100

    Implementing slot scope notion

--- 11-badges-assignment/src/App.vue
+++ 11-badges-assignment/src/App.vue
@@ -3,11 +3,11 @@
     <TheHeader />
     <!-- <the-header></the-header> -> Alternative -->
     <badge-list></badge-list>
-    <user-info
-      :full-name="activeUser.name"
-      :info-text="activeUser.description"
-      :role="activeUser.role"
-    ></user-info>
+    <user-info :full-name="activeUser.name" :info-text="activeUser.description" :role="activeUser.role"></user-info>
+    <course-goals #default="slotProps">
+        <h2>{{ slotProps.item }}</h2>
+        <p>{{ slotProps['anotherProp'] }}</p>
+    </course-goals>
   </div>
 </template>
 
@@ -15,12 +15,14 @@
 import TheHeader from './components/TheHeader.vue';
 import BadgeList from './components/BadgeList.vue';
 import UserInfo from './components/UserInfo.vue';
+import CourseGoals from './components/CourseGoals.vue';
 
 export default {
   components: {
     TheHeader,
     BadgeList,
-    UserInfo
+    UserInfo,
+    CourseGoals,
     // the-header: TheHeader, -> Alternative
   },
   data() {
