b9313f76417453cd8ea19f3a9ded24a49c0afb9f
Anthony <anthony.rousseau26@gmail.com>
2024-03-26 12:15:14 +0100

    Error management if wrong input

--- 11-badges-assignment/src/App.vue
+++ 11-badges-assignment/src/App.vue
@@ -2,31 +2,46 @@
   <div>
     <TheHeader />
     <!-- <the-header></the-header> -> Alternative -->
-    <badge-list></badge-list>
+
+    <!-- <badge-list></badge-list>
     <user-info :full-name="activeUser.name" :info-text="activeUser.description" :role="activeUser.role"></user-info>
     <course-goals #default="slotProps">
         <h2>{{ slotProps.item }}</h2>
         <p>{{ slotProps['anotherProp'] }}</p>
-    </course-goals>
+    </course-goals> -->
+
+    <button @click="setSelectedComponent('active-goals')">Active Goals</button>
+    <button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
+    <!-- <active-goals v-if="selectedComponent === 'active-goals'"></active-goals>
+    <manage-goals v-if="selectedComponent === 'manage-goals'"></manage-goals> -->
+    <keep-alive>
+      <component :is="selectedComponent"></component>
+    </keep-alive>
   </div>
 </template>
 
 <script>
 import TheHeader from './components/TheHeader.vue';
-import BadgeList from './components/BadgeList.vue';
-import UserInfo from './components/UserInfo.vue';
-import CourseGoals from './components/CourseGoals.vue';
+// import BadgeList from './components/BadgeList.vue';
+// import UserInfo from './components/UserInfo.vue';
+// import CourseGoals from './components/CourseGoals.vue';
+import ManageGoals from './components/ManageGoals.vue';
+import ActiveGoals from './components/ActiveGoals.vue';
+
 
 export default {
   components: {
     TheHeader,
-    BadgeList,
-    UserInfo,
-    CourseGoals,
+    // BadgeList,
+    // UserInfo,
+    // CourseGoals,
+    ActiveGoals,
+    ManageGoals,
     // the-header: TheHeader, -> Alternative
   },
   data() {
     return {
+      selectedComponent: 'active-goals',
       activeUser: {
         name: 'Anthony',
         description: 'Training VueJS',
@@ -34,6 +49,11 @@ export default {
       },
     };
   },
+  methods: {
+    setSelectedComponent(cmp) {
+      this.selectedComponent = cmp;
+    }
+  }
 };
 </script>
 
